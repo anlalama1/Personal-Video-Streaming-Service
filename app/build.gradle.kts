341193f79c973f839bdca4772cfb56b0a0216db7
Anthony Lalama <lalamaap1@gmail.com>
2026-09-16 22:18:40 -0700

    Add consistent visual theme across webViewer and Android client(p3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     
     val media3Version = "1.11.0"
     implementation("androidx.media3:media3-exoplayer:$media3Version")
+    implementation("androidx.media3:media3-exoplayer-hls:$media3Version")
     implementation("androidx.media3:media3-ui:$media3Version")
     implementation("androidx.media3:media3-session:$media3Version")
 
